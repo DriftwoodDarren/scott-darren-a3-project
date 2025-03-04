baf14835809e5ce0a8b9040e23f03f1ecb0eb4a5
Darren Scott <000958343@mohawkcollege.ca>
2025-03-04 11:06:33 -0500

    Setup Window

---
+++ scott-darren-a3-project/Game.cs
@@ -0,0 +1,37 @@
+﻿// Include the namespaces (code libraries) you need below.
+using System;
+using System.Numerics;
+
+// The namespace your code is in.
+namespace MohawkGame2D
+{
+    /// <summary>
+    ///     Your game code goes inside this class!
+    /// </summary>
+    public class Game
+    {
+        // Place your variables here:
+
+
+        /// <summary>
+        ///     Setup runs once before the game loop begins.
+        /// </summary>
+        public void Setup()
+        {
+            // Window Setup
+            Window.SetTitle("The Tank Hunt");
+            Window.SetSize(800, 600);
+            Window.TargetFPS = 60;
+            Window.ClearBackground(Color.OffWhite);
+        }
+
+        /// <summary>
+        ///     Update runs every frame.
+        /// </summary>
+        public void Update()
+        {
+
+        }
+    }
+
+}
